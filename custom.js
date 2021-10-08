81c60f9741a5022d03b507a0d4c45f81d6c5b917
tubi55 <tubi55@nate.com>
2021-10-08 16:17:23 +0900

    썸네일 클릭시 유튜브 영상 팝업 호출

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ $.ajax({
     data :{
         part : "snippet", 
         key : "AIzaSyCbARLbftvbPSX5cjABTy1LZpet3RVkrh4", 
-        maxResults : 5, 
+        maxResults : 2, 
         playlistId : "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd"
     }
 })
@@ -49,4 +49,27 @@ $.ajax({
 })
 .error(function(err){
     console.error(err); 
+})
+
+
+//썸네일 클릭시 레이어팝업으로 유튜브 영상 호출하기 
+$("body").on("click", "#vidGallery article a", function(e){
+    e.preventDefault(); 
+
+    let vidId = $(this).attr("href"); 
+
+    $("body")
+        .append(
+            $("<div class='pop'>")
+                .append(
+                    $("<iframe>")
+                        .attr({
+                            src : "https://www.youtube.com/embed/"+vidId,
+                            frameborder : 0,
+                            width:"100%", 
+                            height:600
+                        }), 
+                    $("<span>").text("close")
+                )
+        )
 })
