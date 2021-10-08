bebd1f0becb1c0d0d2dab0b22eae6d09998cf340
tubi55 <tubi55@nate.com>
2021-10-08 15:15:57 +0900

    유튜브 데이터 갯수만큼 콘텐츠 생성

--- custom.js
+++ custom.js
@@ -9,7 +9,33 @@ $.ajax({
     }
 })
 .success(function(data){
-    console.log(data); 
+   // console.log(data); 
+
+    let items = data.items; 
+    console.log(items); 
+
+    //영상 갯수만큼 반복
+    $(items).each(function(index, data){
+
+         
+
+        $("#vidGallery")
+            .append(
+                $("<article>")
+                    .append(
+                        $("<a>").attr({ href : data.snippet.resourceId.videoId })
+                                .append(
+                                    $("<img>").attr({ src : data.snippet.thumbnails.high.url })
+                                ),
+                        $("<div class='con'>")
+                                .append(
+                                    $("<h2>").text(data.snippet.title),
+                                    $("<p>").text(data.snippet.description),
+                                    $("<span>").text(data.snippet.publishedAt)
+                                )
+                    )
+            )
+    });
 })
 .error(function(err){
     console.error(err); 
