e13f7f3c9ce2294b188ea1ed287163eaef2d5cfb
tubi55 <tubi55@nate.com>
2021-10-08 15:46:03 +0900

    텍스트 내용 정리

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ $.ajax({
         part : "snippet", 
         key : "AIzaSyCbARLbftvbPSX5cjABTy1LZpet3RVkrh4", 
         maxResults : 5, 
-        playlistId : "PLYOPkdUKSFgUwLVFuHcpMY2tXYdTJ68dT"
+        playlistId : "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd"
     }
 })
 .success(function(data){
@@ -16,8 +16,18 @@ $.ajax({
 
     //영상 갯수만큼 반복
     $(items).each(function(index, data){
+        
+        //본문 텍스트 내용이 200글자를 넘어가면 말줄임표 붙이기 
+        let txt = data.snippet.description; 
+        let len = txt.length; 
+        if(len>200){
+            txt = txt.substr(0, 200) + "..."
+        } 
 
-         
+        //날짜 텍스트 정리 
+        let date = data.snippet.publishedAt; 
+        date = date.split("T")[0]; 
+        //console.log(date);  
 
         $("#vidGallery")
             .append(
@@ -30,8 +40,8 @@ $.ajax({
                         $("<div class='con'>")
                                 .append(
                                     $("<h2>").text(data.snippet.title),
-                                    $("<p>").text(data.snippet.description),
-                                    $("<span>").text(data.snippet.publishedAt)
+                                    $("<p>").text(txt),
+                                    $("<span>").text(date)
                                 )
                     )
             )
