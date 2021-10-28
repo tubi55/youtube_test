e3a5b4c687d5dcaf48c5dcdec2b61fcc89f6ddba
tubi55 <tubi55@nate.com>
2021-10-28 16:49:14 +0900

    step8 유튜브 데이터 함수처리 및 복수 플레이리스트 불러오기

--- custom.js
+++ custom.js
@@ -1,58 +1,19 @@
-$.ajax({
-    url:"https://www.googleapis.com/youtube/v3/playlistItems",
-    dataType : 'jsonp',
-    data :{
-        part : "snippet", 
-        key : "AIzaSyCbARLbftvbPSX5cjABTy1LZpet3RVkrh4", 
-        maxResults : 2, 
-        playlistId : "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd"
-    }
-})
-.success(function(data){
-   // console.log(data); 
+getYoutube({
+    frame: ".vidGallery",
+    playlist: "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd",
+    num : 4
+});
 
-    let items = data.items;    
 
-    //영상 갯수만큼 반복
-    $(items).each(function(index, data){
-        
-        //본문 텍스트 내용이 200글자를 넘어가면 말줄임표 붙이기 
-        let txt = data.snippet.description; 
-        let len = txt.length; 
-        if(len>200){
-            txt = txt.substr(0, 200) + "..."
-        } 
-
-        //날짜 텍스트 정리 
-        let date = data.snippet.publishedAt; 
-        date = date.split("T")[0]; 
-        //console.log(date);  
-
-        $("#vidGallery")
-            .append(
-                $("<article>")
-                    .append(
-                        $("<a>").attr({ href : data.snippet.resourceId.videoId })
-                                .append(
-                                    $("<img>").attr({ src : data.snippet.thumbnails.high.url })
-                                ),
-                        $("<div class='con'>")
-                                .append(
-                                    $("<h2>").text(data.snippet.title),
-                                    $("<p>").text(txt),
-                                    $("<span>").text(date)
-                                )
-                    )
-            )
-    });
-})
-.error(function(err){
-    console.error(err); 
+getYoutube({
+    frame: ".vidGallery2",
+    playlist: "PLYOPkdUKSFgXiJXlCnCmIoeK_QReBgOlu",
+    num: 4
 })
 
 
 //썸네일 클릭시 레이어팝업으로 유튜브 영상 호출하기 
-$("body").on("click", "#vidGallery article a", function(e){
+$("body").on("click", ".vidGallery article a", function(e){
     e.preventDefault(); 
 
     let vidId = $(this).attr("href"); 
@@ -74,3 +35,55 @@ $("body").on("click", "#vidGallery article a", function(e){
 $("body").on("click", ".pop span", function(){
     $(".pop").remove(); 
 });
+
+function getYoutube(opt){
+    $.ajax({
+        url:"https://www.googleapis.com/youtube/v3/playlistItems",
+        dataType : 'jsonp',
+        data :{
+            part : "snippet", 
+            key : "AIzaSyCbARLbftvbPSX5cjABTy1LZpet3RVkrh4", 
+            maxResults : opt.num, 
+            playlistId : opt.playlist
+        }
+    })
+    .success(function(data){   
+        let items = data.items;  
+        console.log(items);  
+    
+        //영상 갯수만큼 반복
+        $(items).each(function(index, data){
+            //본문 텍스트 내용이 200글자를 넘어가면 말줄임표 붙이기 
+            let txt = data.snippet.description; 
+            let len = txt.length; 
+            if(len>200){
+                txt = txt.substr(0, 200) + "..."
+            } 
+    
+            //날짜 텍스트 정리 
+            let date = data.snippet.publishedAt; 
+            date = date.split("T")[0]; 
+            //console.log(date);  
+    
+            $(opt.frame)
+                .append(
+                    $("<article>")
+                        .append(
+                            $("<a>").attr({ href : data.snippet.resourceId.videoId })
+                                    .append(
+                                        $("<img>").attr({ src : data.snippet.thumbnails.high.url })
+                                    ),
+                            $("<div class='con'>")
+                                    .append(
+                                        $("<h2>").text(data.snippet.title),
+                                        $("<p>").text(txt),
+                                        $("<span>").text(date)
+                                    )
+                        )
+                )
+        });
+    })
+    .error(function(err){
+        console.error(err); 
+    });
+}
