9becf73c388ba301d400ed84f9c2301e5e72b8f5
tubi55 <tubi55@nate.com>
2021-11-19 15:55:30 +0900

    step17 옵션 변경

--- custom.js
+++ custom.js
@@ -1,70 +1,68 @@
-function Youtube(){
-   this.init(); 
-   this.bindingEvent();
+function Youtube(option){
+    this.init(option); 
+    this.bindingEvent();
 }
-Youtube.prototype.init = function(){
-    this.selector = ".vidGallery";
-    this.playlist = "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd";
-    this.key = "AIzaSyCbARLbftvbPSX5cjABTy1LZpet3RVkrh4";
-    this.num = 4;
-}
-Youtube.prototype.bindingEvent = function(){
-    //처음 로딩시 유튜브 데이터 출력
-    this.getYoutube({
-        frame: this.selector,
-        playlist: this.playlist,
-        num : this.num
-    });
+Youtube.prototype.init = function(option){
+    if(option.frame == undefined) {
+        console.error("프레임은 필수 입력값입니다.");
+        return;
+    }
+    if(option.key == undefined) {
+        console.error("키값은 필수 입력값입니다.");
+        return;
+    }
+    if(option.playList == undefined) {
+        option.playList = "PLYOPkdUKSFgX5CgKf68RJzJHec0XEdBNd";
+    }
+    if(option.num == undefined) {
+        option.num = 2;
+    }
 
-    //동적인 리스트의 썸네일 클릭시 팝업호출
+    this.selector = option.frame;
+    this.playlist = option.playList;
+    this.key = option.key;
+    this.num = option.num;
+}
+Youtube.prototype.bindingEvent = function(){  
+    this.getYoutube();
+   
     $("body").on("click", this.selector+" article a", function(e){
         e.preventDefault(); 
         this.createPop(e.currentTarget);
     }.bind(this));
-
-    //동적인 리스트의 제목 클릭시 팝업호출
+   
     $("body").on("click", this.selector+" article h2", function(e){
         this.createPop(e.currentTarget);
     }.bind(this));
-
-    //팝업의 닫기버튼 클릭시 팝업닫기
+   
     $("body").on("click", ".pop span", function(){
         $(".pop").remove(); 
     });
 }
-Youtube.prototype.getYoutube = function(opt){
-    if(opt.frame===undefined) console.error("frame속성값은 필수 입력사항입니다.");
-    if(opt.playlist===undefined) opt.playlist = "PLYOPkdUKSFgWPLsAWpqRpK0cCiAGdxi-Y";
-    if(opt.num===undefined) opt.num = 2;
-
+Youtube.prototype.getYoutube = function(){  
     $.ajax({
         url:"https://www.googleapis.com/youtube/v3/playlistItems",
         dataType : 'jsonp',
         data :{
             part : "snippet", 
             key : this.key, 
-            maxResults : opt.num, 
-            playlistId : opt.playlist
+            maxResults : this.num, 
+            playlistId : this.playlist
         }
     })
     .success(function(data){   
-        let items = data.items;         
-    
-        //영상 갯수만큼 반복
+        let items = data.items;  
+
         $(items).each(function(_, data){
-            //본문 텍스트 내용이 200글자를 넘어가면 말줄임표 붙이기 
             let txt = data.snippet.description; 
             let len = txt.length; 
-            if(len>200){
-                txt = txt.substr(0, 200) + "..."
-            } 
-    
-            //날짜 텍스트 정리 
+            if(len>200) txt = txt.substr(0, 200) + "...";
+            
             let date = data.snippet.publishedAt; 
             date = date.split("T")[0]; 
-            //console.log(date);  
+      
     
-            $(opt.frame)
+            $(this.selector)
                 .append(
                     $("<article>")
                         .append(
@@ -83,8 +81,8 @@ Youtube.prototype.getYoutube = function(opt){
                                     )
                         )
                 )
-        });
-    })
+        }.bind(this)); //each문 this 고정
+    }.bind(this)) //ajax문 고정
     .error(function(err){
         console.error(err); 
     });
